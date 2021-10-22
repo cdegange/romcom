10c39c311c5af60f51f9b6f6cac548474ed78e2a
Chad DeGange <chaddegange@gmail.com>
2021-10-22 18:35:17 -0400

    Add home view,fix save cover button

--- src/main.js
+++ src/main.js
@@ -1,33 +1,23 @@
-<<<<<<< HEAD
-// Create variables targetting the relevant DOM elements here 👇
-var getRandomCoverButton = document.querySelector(".random-cover-button")
-=======
 // Create variables targetting the relevant DOM elements here :point_down:
 
 var getRandomCoverButton = document.querySelector(".random-cover-button")
 var currentImage = document.querySelector(".cover-image");
-<<<<<<< HEAD
+
 /// i think follow this guideline for the rest of the classes
->>>>>>> main
-=======
+
 var currentTitle = document.querySelector(".cover-title");
 var currentTag1 = document.querySelector(".tagline-1");
 var currentTag2 = document.querySelector(".tagline-2");
 
-<<<<<<< HEAD
-var makeCoverButton = document.querySelector(".view form-view hidden");
->>>>>>> main
-=======
 var makeCoverButton = document.querySelector(".make-new-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
 var homeButton = document.querySelector(".home-button");
 var viewForm = document.querySelector(".form-view");
 var homeView = document.querySelector(".home-view");
+var viewSavedButton = document.querySelector(".view-saved-button")
+var savedCoversView = document.querySelector(".saved-view");
 
-var savedCoversView = document.querySelector(".saved-covers-section");
 
-
->>>>>>> main
 
 // We've provided a few variables below
 var savedCovers = [
@@ -35,28 +25,18 @@ var savedCovers = [
 ];
 var currentCover;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Add your event listeners here 👇
-
-getRandomCoverButton.addEventListener("click", createRandomCover)
-
-// Create your event handlers and other functions here 👇
-
-=======
 // Add your event listeners here :point_down:
 getRandomCoverButton.addEventListener("click", createRandomCover)
->>>>>>> main
-=======
+
 
 // Add your event listeners here :point_down:
 window.addEventListener("load", createRandomCover);
 getRandomCoverButton.addEventListener("click", createRandomCover);
-
+homeButton.addEventListener("click", phoneHome)
 makeCoverButton.addEventListener("click", hideAndShow);
->>>>>>> main
 
-saveCoverButton.addEventListener("click", viewSavedPage);
+
+viewSavedButton.addEventListener("click", viewSavedPage);
 
 // Create your event handlers and other functions here :point_down:
 // We've provided one function to get you started
@@ -69,22 +49,13 @@ function createRandomCover() {
   var newTitle = titles[getRandomIndex(titles)];
   var newTagline1 = descriptors[getRandomIndex(descriptors)];
   var newTagline2 = descriptors[getRandomIndex(descriptors)];
-<<<<<<< HEAD
-
-  currentCover = new Cover(newCover, newTitle, newTagline1, newTagline2);
-}
-
-currentCover = document.querySelector(".cover-image");
-=======
   var cover = new Cover(newCover, newTitle, newTagline1, newTagline2);
   currentImage.src = cover.cover;
   currentTitle.innerText = cover.title;
   currentTag1.innerText = cover.tagline1;
   currentTag2.innerText = cover.tagline2;
 }
-<<<<<<< HEAD
->>>>>>> main
-=======
+
 
 function hideAndShow() {
   getRandomCoverButton.classList.add("hidden");
@@ -104,6 +75,14 @@ function viewSavedPage() {
   savedCoversView.classList.remove("hidden");
 }
 
+function phoneHome() {
+  viewForm.classList.add("hidden");
+  homeView.classList.remove("hidden");
+  getRandomCoverButton.classList.remove("hidden");
+  saveCoverButton.classList.remove("hidden");
+  homeButton.classList.add("hidden");
+}
+
 // 1. create a variable targeting make your own cover button
 // 2. when button is clicked, the homepage view should be hidden and the
 // create your own cover form should be shown - create event listener and function
@@ -111,4 +90,3 @@ function viewSavedPage() {
 // 4. home button should still be visible
 // 5. set up event listener for view saved covers button
 // 6.
->>>>>>> main
